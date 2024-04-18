058b8850070b71295ab909d451c1c7ddbca3ae7b
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-18 23:26:04 +0300

    just used first task go_nodes_function, and added level var, so that you can store its level of position, then sorted dict, and return list with Nodes, values, worked at first time :)

--- Sort binary tree by levels/sort_binary_tree_by_levels.py
+++ Sort binary tree by levels/sort_binary_tree_by_levels.py
@@ -1,1 +1,19 @@
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
 
+def tree_by_levels(node):
+    if not node:
+        return []
+    all_nodes = {}
+    def go_nodes(node, level=0):
+        if node:
+            all_nodes[node] = level
+            go_nodes(node.left, level + 1)
+            go_nodes(node.right, level + 1)
+    go_nodes(node, level=0)
+    all_nodes = sorted(all_nodes.items(), key= lambda x: x[1])
+    all_nodes2 = [i.value for i, j in all_nodes]
+    return all_nodes2
