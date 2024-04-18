0ce029a8e733d9af3162244d4248d92e3fad72c3
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-18 20:27:06 +0300

    pre-order вийшло з першого разу, просто достатньо було зробити рекурсію, яка б спочатку йшла вліво, потім вправо

--- Binary Tree Traversal/binary_tree_traversal.py
+++ Binary Tree Traversal/binary_tree_traversal.py
@@ -1,1 +1,24 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+def pre_order(node):
+    all_nodes = []
+    def go_nodes(node):
+        if node:
+            all_nodes.append(node.data)
+            go_nodes(node.left)
+            go_nodes(node.right)
+    go_nodes(node)
+    return all_nodes
 
+
+a = Node(5)
+b = Node(10)
+c = Node(2)
+d = Node("leaf")
+a.left = b
+a.right = c
+c.left = d
+print(pre_order(a))
