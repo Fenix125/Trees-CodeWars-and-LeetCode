732ed4ab516cc041532b9b2966b1194d79c00823
Mykhailo_Ivasiuk <migelspainn@gmail.com>
2024-04-19 11:23:27 +0300

    made some code that recursively iterates and not deletes, but redefines node to delete value with the biggest one, but still does not work, because it needs to delete used nodes

--- Delete Node in a BST/delete_node_in_bst.py
+++ Delete Node in a BST/delete_node_in_bst.py
@@ -15,7 +15,7 @@ def tree_by_levels(node):
             go_nodes(node.right, level + 1)
     go_nodes(node, level=0)
     all_nodes = sorted(all_nodes.items(), key= lambda x: x[1])
-    all_nodes2 = [i.value for i, j in all_nodes]
+    all_nodes2 = [i.val for i, j in all_nodes]
     return all_nodes2
 
 class Solution:
@@ -24,13 +24,36 @@ class Solution:
         def go_nodes(node, parent):
             if node:
                 if node.val == key:
-                    print('found')
+                    if not node.left and not node.right:
+                        if parent.left.val == node.val:
+                            parent.left = None
+                        else:
+                            parent.right = None
+                    elif not node.left and node.right:
+                        if parent.left.val == node.val:
+                            parent.left = node.right
+                        else:
+                            parent.right = node.right
+                    elif not node.right and node.left:
+                        if parent.left.val == node.val:
+                            parent.left = node.left
+                        else:
+                            parent.right = node.left
+                    elif node.right and node.left:
+                        parent_copy = node
+                        biggest = node.right
+                        while biggest.left:
+                            parent_copy = biggest
+                            biggest = biggest.left
+                        node.val = biggest.val
+                        go_nodes(biggest, parent_copy)
                 else:
                     if node.val > key:
                         go_nodes(node.left, node)
                     elif node.val < key:
                         go_nodes(node.right, node)
         go_nodes(copy_root, copy_root)
+        return tree_by_levels(copy_root)
 
     
 root = TreeNode(5, TreeNode(3, left=TreeNode(2), right=TreeNode(4)), TreeNode(6, right=TreeNode(7)))
